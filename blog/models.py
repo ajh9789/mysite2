d70ef85dd69c05cf5106f0b207960c486ce0a1a5
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-07-26 10:53:17 +0900

    model makemigration migrate

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Post(models.Model) :
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_data='publish', allow_unicode=True)
-    body = models.CharField(max_length=1000)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    class Meta :
-        ordering = ['created']
-        indexs = [
-            models.Index(fields=['created'])
-        ]
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta :
+        ordering = ['published']
+        indexes = [
+            models.Index(fields=['-published'])
+        ]
+    def __str__(self):
+        return self.title
