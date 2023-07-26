c40afeeb2b820af3532ad2561a41139c7e527f8c
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-07-26 10:28:06 +0900

    직접작성 포스트모델 메타 클래스 status클래스

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model) :
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_data='publish', allow_unicode=True)
+    body = models.CharField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta :
+        ordering = ['created']
+        indexs = [
+            models.Index(fields=['created'])
+        ]
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
