95f4be6930a54e5fd79cd92afe1052b71e611572
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-07-27 10:29:37 +0900

    이메일 연동 이메일폼 추가

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
